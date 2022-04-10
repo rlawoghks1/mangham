15a40e10ee6ad34bc85d3cc8606554568039f42a
rlawoghks1 <rla1269a@gmail.com>
2022-04-10 17:11:19 +0900

    4월 10일 까지의 작업

--- components/pages/Main/Main.js
+++ components/pages/Main/Main.js
@@ -2,9 +2,8 @@ import React, { useState, useRef, useEffect } from "react";
 import styled from "styled-components";
 import { useRecoilState } from "recoil";
 import { isLoggedInState } from "./../../../recoil/Global";
-import { View, FlatList, Text, Button } from "react-native";
+import { View, FlatList, Text, Button, StyleSheet,Image } from "react-native";
 import api from "./../../../config/Api";
-// import SimpleImageSlider from "react-simple-image-slider/dist/ImageSlider";
 
 const Container = styled.View`
   width: 100%;
@@ -13,21 +12,78 @@ const Container = styled.View`
   margin: 5% 0;
 `;
 
-const ContainerText = styled.Text`
-  font-size: 18px;
-  color: gray;
-  padding: 5%;
-`;
-
 const ContainterHead = styled.Text`
-  padding: 5%;
-  font-size: 40px;
+  padding: 20px 0 0 20px;
+  font-size: 24px;
 `;
 
-const FlexDemo = () => {
+const ContainerText = styled.Text`
+  font-size: 14px;
+  color: gray;
+  padding: 10px 0 0 20px;
+`;
+
+const Image1 = styled.View`
+  width: 60%;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 60px;
+  margin-top : 5px;
+  position : relative;
+`;
+
+const BackImageView = styled.ImageBackground`
+  width: 250px;
+  height: 60px;
+  flex-direction: column;
+`;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  logo: {
+    width: 66,
+    height: 58,
+  },
+});
+
+// const Image2 = () => {
+//   return (
+//     <View style={styles.container}>
+//       <Image
+//         style={styles.tinyLogo}
+//         source={require("./../../assets/stew.png")}
+//       />
+//       <Image
+//         style={styles.tinyLogo}
+//         source={require("./../../assets/store.png")}
+//       />
+//       <Image
+//         style={styles.logo}
+//         source={require("./../../assets/cafe.png")}
+//       />
+//     </View>
+//   );
+// }
+
+
+const Flexbox = () => {
   return (
-    <View style={{ marginTop: 30, height: '3%'}}>
+    <View style={{ marginTop: 10, height: 10}}>
       <View style={{ flex: 1, backgroundColor: '#c4c4c4'}} />
+    </View>
+
+  )
+}
+
+const Demobox = () => {
+  return (
+    <View style={{ height: 30}}>
     </View>
 
   )
@@ -59,42 +115,93 @@ const Main = ({ navigation, route }) => {
     }
   }
 
-const App = () => {
-  return (
-    <div>
-      <SimpleImageSlider
-        width={896}
-        height={504}
-        images={images}
-        showBullets={true}
-        shoWNavs={ture}
-        />
-    </div>
-  );
-}
+
+
 
   useEffect(() => {
     getData();
   }, []);
 
-  const renderItem = ({ item }) => (
+  const RenderItem = ({ item }) => (
     <View>
       <Text>{item.name}</Text>
     </View>
   );
 
-
+  const Stew = () => {
+    return (
+      <View style={styles.container}>
+        <Image
+          style={{
+            borderColor: "gray",
+            borderWidth: 1,
+            height: 100,
+            width: 100
+          }}
+          source={require("./../../../assets/stew.png")}
+        />
+      </View>
+    );
+  }
   // statusBar.setBackgroundColor("transparent");
   // statusBar.setTranslucent(true);
   // statusBar.setBarstyle("dark-content");
-  
+  const RebornText = () => {
+    return (
+      <Container>
+        <BackImageView />
+      </Container>
+    )
+  }
+
+
+
   return (
     <Container>
       <ContainterHead>김재환님</ContainterHead>
       <ContainerText>오늘 일정은 어떠셨나요</ContainerText>
-      <FlexDemo></FlexDemo>
+      <Flexbox></Flexbox>
+      <Image1>
+        <BackImageView source={require("./../../../assets/reborn.png")} resizeMode="stretch"/>
+        <Text> 오늘 일정은?</Text>
+        </Image1>
+      <ContainerText> 저녁 식사 준비하기</ContainerText>
+        <View 
+      style={{
+        flex:1,
+        flexDirection:"row",
+        justifyContent:"space-around",
+      }}
+    >
+      <View 
+      
+      // <Stewimage source={require("./../../../assets/reborn.png")} resizeMode="stretch"/>
+        style={{
+          backgroundColor:"green",
+          width:80,
+          height:80,
+        }}
+      /> 
+      <View 
+        style={{
+          backgroundColor:"gold",
+          width:80,
+          height:80,
+        }}
+      /> 
+      <View 
+        style={{
+          backgroundColor:"tomato",
+          width:80,
+          height:80,
+        }}
+      /> 
+    </View>
+    <Demobox></Demobox>
+    
     </Container>
   );
 };
+
 
 export default Main;
